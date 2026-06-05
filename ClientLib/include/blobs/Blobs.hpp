a39476b0581369f29879a8d438d72a70a00929a5
lSoleyl <lSoleyl+github@gmail.com>
2026-06-05 21:57:47 +0200

    Added THIRD-PARTY-NOTICES for doctest
     - Also added a copyright comment to Blobs.hpp as suggested

--- ClientLib/include/blobs/Blobs.hpp
+++ ClientLib/include/blobs/Blobs.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+// Blobs.hpp - central include header for blobs.db database client library
+//
+// Copyright (c) 2025-2026 David Olszowka
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE
+
+
 /** This header will include all of blobs.db
  */
 #include "Initialization.hpp"
